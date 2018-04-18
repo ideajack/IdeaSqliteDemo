c8a563a1dc77ce0c31d6c7c0f77ce8ac8ecab8f6
idea_zxj <jack_eloise@163.com>
2018-04-18 22:54:16 +0800

    upgrade db

--- app/src/main/java/com/jack/ideasqlitedemo/MainActivity.java
+++ app/src/main/java/com/jack/ideasqlitedemo/MainActivity.java
@@ -1,16 +1,50 @@
 package com.jack.ideasqlitedemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+
+import com.jack.sqlite.IdeaDatebaseHelper;
 
 /**
  * Sqlite数据库的基本操作
+ * （经过很长时间的android开发，会发现android开发人员（包括自己）只是会一些android技术，很少了解java
+ * 后端的技术的话，对于自己的后续发展是很不利的）
+ * 数据库技术在后端开发中很重要，在android开发中也占有很重要的角色
+ * 因此，一定要掌握，并熟练运用。
+ * （sp和文件只适用于保存一些简单的数据和键值对）
+ * （数据库可以用来保存 数据量大，结构性复杂的数据）
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    private IdeaDatebaseHelper mDbHelper;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        mDbHelper = new IdeaDatebaseHelper(this, "BookStore.db", null, 2);
+        findViewById(R.id.create_table).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create database
+                mDbHelper.getWritableDatabase();
+            }
+        });
+
+        findViewById(R.id.update_db).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //update db
+                //onUpgrade() 中操作
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/jack/sqlite/IdeaDatebaseHelper.java
@@ -0,0 +1,46 @@
+package com.jack.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by idea_jack on 2017/11/27.
+ */
+
+public class IdeaDatebaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table book (" +
+            "id integer primary key autoincrement, " +
+            "name text, " +
+            "author text, " +
+            "price real, " +
+            "pages integer)";
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement,"
+            + "category_name text,"
+            + "category_code)";
+    private Context mContext;
+
+    public IdeaDatebaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);//create table
+        db.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext, "create table", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+
+        Toast.makeText(mContext, "upgrade db", Toast.LENGTH_SHORT).show();
+    }
+}
